19327e6939c8ceb503ae2a40022b0814eef5988b
Mihir Patel <mihirpatel.mp.1112@gmail.com>
2025-03-31 14:54:17 +1100

    Overflow problem fixed

--- pages/_components/TileCards.js
+++ pages/_components/TileCards.js
@@ -27,7 +27,7 @@ export default function TileCards() {
   return (
     <div className="bg-tertiary">
     <Margin>
-      <div className="flex justify-between py-36 gap-x-14 overflow-auto sm:overflow-hidden snap-x snap-mandatory">
+      <div className="flex justify-between py-36 gap-x-14 overflow-auto lg:overflow-hidden">
         {cardData.map((card, index) => {
           return (
             <Card
