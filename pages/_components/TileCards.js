98ed55c292967ed6c88e2b9ededd3437c7651b24
Mihir Patel <mihirpatel.mp.1112@gmail.com>
2025-03-31 21:13:08 +1100

    Grid added

--- pages/_components/TileCards.js
+++ pages/_components/TileCards.js
@@ -26,22 +26,27 @@ const cardData = [
 export default function TileCards() {
   return (
     <div className="bg-tertiary">
-    <Margin>
-    <div className="flex flex-col sm:flex-row justify-center sm:justify-between py-36 gap-y-10 sm:gap-x-14">
-        {cardData.map((card, index) => {
-          return (
-            <Card
-              key={index}
-              title={card.title}
-              body={card.body}
-              imageURL={card.imageURL}
-              imageAlt={card.imageAlt}
-              className="snap-center"
-            />
-          );
-        })}
-      </div>
-    </Margin>
+  <Margin>
+    <div className="
+      grid
+      grid-cols-1          
+      gap-6              
+      md:grid-cols-2     
+      xl:grid-cols-3     
+      py-36
+    ">
+      {cardData.map((card, index) => (
+        <Card
+          key={index}
+          title={card.title}
+          body={card.body}
+          imageURL={card.imageURL}
+          imageAlt={card.imageAlt}
+          className="snap-center"
+        />
+      ))}
     </div>
+  </Margin>
+</div>
   );
 }
